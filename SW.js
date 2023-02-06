6e4136a1dc3a13f6578724671c9ed9f5dcfba0f1
anton rångeby <antonrangeby@MacBook-Air-som-tillhor-anton-2.local>
2023-02-06 16:12:17 +0100

    mycket fungerar nu

--- SW.js
+++ SW.js
@@ -12,6 +12,9 @@ const input = document.getElementById("searchInput");
 const allCharacters = "https://swapi.dev/api/people/";
 const pElement = document.querySelector("p");
 const inputContainer = document.querySelector("#InputContainer");
+const suggestions = document.getElementById("suggestions");
+const favorites = document.getElementById("favourites");
+const selectedCharCards = document.querySelector("#selectedCharCards");
 let charList = {};
 let pages = {};
 for (let i = 0; i <= 9; i++) {
@@ -34,6 +37,7 @@ function characters() {
             test.push(list.results);
         }
         createPerson();
+        consolebuttons();
     });
 }
 characters();
@@ -52,19 +56,33 @@ function createPerson() {
         }
     }
     buttonMaker();
+    charCards();
 }
 // skapar buttons för alla personer som vi skapat och lägger till namnet på person innuti
 function buttonMaker() {
     for (let i = 0; i < persons.length; i++) {
         let newbuttons = document.createElement("button");
-        newbuttons.classList.add("hidden", "charButtons");
+        newbuttons.classList.add("charButtons", "hidden");
         newbuttons.textContent = persons[i].name;
-        inputContainer.appendChild(newbuttons);
+        suggestions.appendChild(newbuttons);
+    }
+}
+function charCards() {
+    for (let i = 0; i < persons.length; i++) {
+        let newCard = document.createElement("div");
+        let newCardName = document.createElement("p");
+        newCardName.innerHTML = persons[i].name;
+        let newCardBirthYear = document.createElement("p");
+        newCardBirthYear.innerHTML = persons[i].birth_year;
+        newCard.appendChild(newCardName);
+        newCard.appendChild(newCardBirthYear);
+        newCard.classList.add("charCards", "hidden");
+        selectedCharCards.appendChild(newCard);
     }
 }
 input.addEventListener("keyup", function (event) {
     var _a;
-    const buttons = document.querySelectorAll("button");
+    const buttons = document.querySelectorAll(".charButtons");
     if (!input.value) {
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].classList.add("hidden");
@@ -81,3 +99,39 @@ input.addEventListener("keyup", function (event) {
         }
     }
 });
+let lastClickedButton;
+function consolebuttons() {
+    const charButtons = document.querySelectorAll(".charButtons");
+    const charCards = document.querySelectorAll(".charCards");
+    charButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const person = persons.find((p) => p.name === button.innerText);
+            if (person) {
+                input.value = "";
+                charCards.forEach((card) => {
+                    card.classList.add("hidden");
+                    const cardName = card.querySelector("p:first-child");
+                    if (cardName.innerText === person.name) {
+                        card.classList.remove("hidden");
+                    }
+                });
+            }
+        });
+    });
+}
+// TESTA SENARE 
+// const favoritKnapp = document.querySelector("#favoritKnapp") as HTMLButtonElement;
+// favoritKnapp.onclick = function() {
+//     console.log("hej");
+//     const favorites = document.getElementById("favourites") as HTMLDivElement;
+//     if (lastClickedButton) {
+//         const copyButton = lastClickedButton.cloneNode(true) as HTMLButtonElement;
+//         copyButton.classList.remove("charButtons", "hidden");
+//         copyButton.classList.add("favCharacters");
+//         if (lastClickedButton.onclick) {
+//             copyButton.addEventListener("click", lastClickedButton.onclick);
+//         }
+//         favorites.appendChild(copyButton);
+//     }
+// }
+//----------------------------------------------------------------------------------------------------------//
--- SW.ts
+++ SW.ts
@@ -2,8 +2,9 @@ const input = document.getElementById("searchInput") as HTMLInputElement;
 const allCharacters = "https://swapi.dev/api/people/";
 const pElement = document.querySelector("p") as HTMLParagraphElement;
 const inputContainer = document.querySelector("#InputContainer") as HTMLDivElement;
-
-
+const suggestions = document.getElementById("suggestions") as HTMLDivElement;
+const favorites = document.getElementById("favourites") as HTMLDivElement;
+const selectedCharCards = document.querySelector("#selectedCharCards") as HTMLDivElement;
 
 let charList: any = {};
 let pages: any = {};
@@ -27,6 +28,7 @@ async function characters() {
     test.push(list.results);
   }
   createPerson()
+  selectCharacters()
   
 }
 characters()
@@ -51,20 +53,33 @@ function createPerson() {
     }
 }
 buttonMaker()
+charCards()
 }
 // skapar buttons för alla personer som vi skapat och lägger till namnet på person innuti
 function buttonMaker() {
     for (let i = 0; i < persons.length; i++) {
       let newbuttons = document.createElement("button") as HTMLButtonElement;
-      newbuttons.classList.add("hidden", "charButtons");
+      newbuttons.classList.add("charButtons", "hidden");
       newbuttons.textContent = persons[i].name;
-      inputContainer.appendChild(newbuttons);
+      suggestions.appendChild(newbuttons);
+    }
+  }
+  function charCards() {
+    for (let i = 0; i < persons.length; i++) {
+      let newCard = document.createElement("div") as HTMLDivElement;
+      let newCardName = document.createElement("p") as HTMLParagraphElement;
+      newCardName.innerHTML = persons[i].name;
+      let newCardBirthYear = document.createElement("p") as HTMLParagraphElement;
+      newCardBirthYear.innerHTML = persons[i].birth_year;
+      newCard.appendChild(newCardName);
+      newCard.appendChild(newCardBirthYear);
+      newCard.classList.add("charCards", "hidden")
+      selectedCharCards.appendChild(newCard)
     }
   }
 
-
   input.addEventListener("keyup", function(event) {
-    const buttons = document.querySelectorAll("button") as NodeListOf<HTMLButtonElement>;
+    const buttons = document.querySelectorAll(".charButtons") as NodeListOf<HTMLButtonElement>;
     if (!input.value) {
       for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.add("hidden");
@@ -82,4 +97,52 @@ function buttonMaker() {
   });
 
 
- 
+
+
+  let lastClickedButton: HTMLButtonElement;
+
+  function selectCharacters() {
+    const charButtons = document.querySelectorAll(".charButtons") as NodeListOf<HTMLButtonElement>;
+    const charCards = document.querySelectorAll(".charCards") as NodeListOf<HTMLDivElement>;
+    charButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const person = persons.find((p) => p.name === button.innerText);
+  
+        if (person) {
+          input.value = "";
+            
+          charCards.forEach((card) => {
+            card.classList.add("hidden");
+            const cardName = card.querySelector("p:first-child") as HTMLParagraphElement;
+            if (cardName.innerText === person.name) {
+              card.classList.remove("hidden");
+            }
+          });
+        }
+      });
+    });
+  }
+// TESTA SENARE 
+// const favoritKnapp = document.querySelector("#favoritKnapp") as HTMLButtonElement;
+
+// favoritKnapp.onclick = function() {
+//     console.log("hej");
+//     const favorites = document.getElementById("favourites") as HTMLDivElement;
+    
+//     if (lastClickedButton) {
+//         const copyButton = lastClickedButton.cloneNode(true) as HTMLButtonElement;
+//         copyButton.classList.remove("charButtons", "hidden");
+//         copyButton.classList.add("favCharacters");
+//         if (lastClickedButton.onclick) {
+//             copyButton.addEventListener("click", lastClickedButton.onclick);
+//         }
+//         favorites.appendChild(copyButton);
+//     }
+// }
+//----------------------------------------------------------------------------------------------------------//
+
+
+
+
+
+
