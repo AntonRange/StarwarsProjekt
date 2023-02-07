a17df2587849bb5618e0498fb1c9f068a75a22e8
anton rångeby <antonrangeby@MacBook-Air-som-tillhor-anton-2.local>
2023-02-07 02:18:27 +0100

    Mycket fungerar nu, knapparna hittar rätt charCard äntligen. Kör en commit innan jag börjar leka med promise all för jag vet att koden kommer krångla om jag börjar med detgit add .

--- SW.js
+++ SW.js
@@ -34,26 +34,42 @@ function characters() {
             listOfCharacters = yield fetch(pages[i]);
             console.log(`Loading page ${i}`);
             let list = yield listOfCharacters.json();
-            test.push(list.results);
+            for (const character of list.results) {
+                const response = yield fetch(character.homeworld);
+                const homeworld = yield response.json();
+                let filmTitles = [];
+                for (let j = 0; j < character.films.length; j++) {
+                    const film = yield fetch(character.films[j]);
+                    const filmData = yield film.json();
+                    filmTitles.push(filmData.title);
+                }
+                test.push({
+                    name: character.name,
+                    birth_year: character.birth_year,
+                    homeworld: homeworld.name,
+                    films: filmTitles
+                });
+            }
         }
         createPerson();
-        consolebuttons();
+        selectCharacters();
     });
 }
 characters();
 class Person {
-    constructor(name, birth_year) {
+    constructor(name, birth_year, homeWorld, films) {
         this.name = name;
         this.birth_year = birth_year;
+        this.homeWorld = homeWorld;
+        this.films = films;
     }
 }
 let persons = [];
 function createPerson() {
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < test[i].length; j++) {
-            let newPerson = new Person(test[i][j].name, test[i][j].birth_year);
-            persons.push(newPerson);
-        }
+    console.log("reeeee");
+    for (let j = 0; j < test.length; j++) {
+        let newPerson = new Person(test[j].name, test[j].birth_year, test[j].homeworld, test[j].films);
+        persons.push(newPerson);
     }
     buttonMaker();
     charCards();
@@ -67,19 +83,27 @@ function buttonMaker() {
         suggestions.appendChild(newbuttons);
     }
 }
+// skapar "Cards" för varje person. En sektion och två p taggar. Ska uppdateras med mer! TESTVERSION
 function charCards() {
     for (let i = 0; i < persons.length; i++) {
         let newCard = document.createElement("div");
         let newCardName = document.createElement("p");
-        newCardName.innerHTML = persons[i].name;
+        newCardName.innerHTML = `Name: ${persons[i].name}`;
         let newCardBirthYear = document.createElement("p");
-        newCardBirthYear.innerHTML = persons[i].birth_year;
+        newCardBirthYear.innerHTML = `Birth Year: ${persons[i].birth_year}`;
+        let newCardFilms = document.createElement("p");
+        newCardFilms.innerHTML = `Starred in`;
+        for (let j = 0; j < persons[i].films.length; j++) {
+            newCardFilms.innerHTML += `${persons[i].films[j]}, `;
+        }
         newCard.appendChild(newCardName);
         newCard.appendChild(newCardBirthYear);
+        newCard.appendChild(newCardFilms);
         newCard.classList.add("charCards", "hidden");
         selectedCharCards.appendChild(newCard);
     }
 }
+// lyssnar på inputfielden på knapptryck. För varje knapp som trycks så jämför den inputvärdet med knapparnas textcontent. Tex om du skriver "lu" så kommer den hitta alla som har "lu" i knappens innertext.
 input.addEventListener("keyup", function (event) {
     var _a;
     const buttons = document.querySelectorAll(".charButtons");
@@ -90,7 +114,9 @@ input.addEventListener("keyup", function (event) {
     }
     else {
         for (let i = 0; i < buttons.length; i++) {
+            // här sker jämförelsen
             if (buttons[i].textContent && ((_a = buttons[i].textContent) === null || _a === void 0 ? void 0 : _a.toLowerCase().includes(input.value.toLowerCase()))) {
+                //så om if satsen hittar matching så tar den bort klassen hidden från knappen så att den visas i suggestions sectionen. Om en knapp inte matchar texten man skrivit in i inputfielden så får den hidden klassen
                 buttons[i].classList.remove("hidden");
             }
             else {
@@ -99,39 +125,17 @@ input.addEventListener("keyup", function (event) {
         }
     }
 });
-let lastClickedButton;
-function consolebuttons() {
+function selectCharacters() {
     const charButtons = document.querySelectorAll(".charButtons");
     const charCards = document.querySelectorAll(".charCards");
-    charButtons.forEach((button) => {
-        button.addEventListener("click", () => {
-            const person = persons.find((p) => p.name === button.innerText);
-            if (person) {
-                input.value = "";
-                charCards.forEach((card) => {
-                    card.classList.add("hidden");
-                    const cardName = card.querySelector("p:first-child");
-                    if (cardName.innerText === person.name) {
-                        card.classList.remove("hidden");
-                    }
-                });
+    for (let i = 0; i < charButtons.length; i++) {
+        charButtons[i].onclick = function () {
+            // Hide all charCards
+            for (let j = 0; j < charCards.length; j++) {
+                charCards[j].style.display = "none";
             }
-        });
-    });
+            // Show the corresponding charCard
+            charCards[i].style.display = "block";
+        };
+    }
 }
-// TESTA SENARE 
-// const favoritKnapp = document.querySelector("#favoritKnapp") as HTMLButtonElement;
-// favoritKnapp.onclick = function() {
-//     console.log("hej");
-//     const favorites = document.getElementById("favourites") as HTMLDivElement;
-//     if (lastClickedButton) {
-//         const copyButton = lastClickedButton.cloneNode(true) as HTMLButtonElement;
-//         copyButton.classList.remove("charButtons", "hidden");
-//         copyButton.classList.add("favCharacters");
-//         if (lastClickedButton.onclick) {
-//             copyButton.addEventListener("click", lastClickedButton.onclick);
-//         }
-//         favorites.appendChild(copyButton);
-//     }
-// }
-//----------------------------------------------------------------------------------------------------------//
--- SW.ts
+++ SW.ts
@@ -18,39 +18,61 @@ for (let i = 0; i <= 9; i++) {
 
 let listOfCharacters: any = [];
 let test: any = [];
+
 async function characters() {
-    
-  for (let i = 1; i <= 9; i++) {
-    console.log(pages[i])
-    listOfCharacters = await fetch(pages[i]);
-    console.log(`Loading page ${i}`);
-    let list = await listOfCharacters.json();
-    test.push(list.results);
-  }
-  createPerson()
-  selectCharacters()
+    for (let i = 1; i <= 9; i++) {
+      console.log(pages[i]);
+      listOfCharacters = await fetch(pages[i]);
+      console.log(`Loading page ${i}`);
+      let list = await listOfCharacters.json();
   
-}
+      for (const character of list.results) {
+        const response = await fetch(character.homeworld);
+        const homeworld = await response.json();
+  
+        let filmTitles = [];
+        for (let j = 0; j < character.films.length; j++) {
+          const film = await fetch(character.films[j]);
+          const filmData = await film.json();
+          filmTitles.push(filmData.title);
+        }
+  
+        test.push({
+          name: character.name,
+          birth_year: character.birth_year,
+          homeworld: homeworld.name,
+          films: filmTitles
+        });
+      }
+    }
+    createPerson()
+    selectCharacters()
+  }
 characters()
+
+
+
+
 
 class Person {
     name: string;
     birth_year: string;
+    homeWorld: string;
+    films: string;
   
-    constructor(name: string, birth_year: string) {
+    constructor(name: string, birth_year: string, homeWorld: string, films: string) {
       this.name = name;
       this.birth_year = birth_year;
+      this.homeWorld = homeWorld;
+      this.films = films;
     }
   }
 let persons: Person[] = [];
 function createPerson() {
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < test[i].length; j++) {
-      let newPerson = new Person(test[i][j].name, test[i][j].birth_year);
+    console.log("reeeee");
+    for (let j = 0; j < test.length; j++) {
+      let newPerson = new Person(test[j].name, test[j].birth_year, test[j].homeworld, test[j].films);
       persons.push(newPerson);
-        
-        
-    }
 }
 buttonMaker()
 charCards()
@@ -64,20 +86,28 @@ function buttonMaker() {
       suggestions.appendChild(newbuttons);
     }
   }
+
+  // skapar "Cards" för varje person. En sektion och två p taggar. Ska uppdateras med mer! TESTVERSION
   function charCards() {
     for (let i = 0; i < persons.length; i++) {
       let newCard = document.createElement("div") as HTMLDivElement;
       let newCardName = document.createElement("p") as HTMLParagraphElement;
-      newCardName.innerHTML = persons[i].name;
+      newCardName.innerHTML = `Name: ${persons[i].name}`;
       let newCardBirthYear = document.createElement("p") as HTMLParagraphElement;
-      newCardBirthYear.innerHTML = persons[i].birth_year;
+      newCardBirthYear.innerHTML = `Birth Year: ${persons[i].birth_year}`;
+      let newCardFilms = document.createElement("p") as HTMLParagraphElement;
+      newCardFilms.innerHTML = `Starred in: `;
+      for (let j = 0; j < persons[i].films.length; j++) {
+        newCardFilms.innerHTML += `${persons[i].films[j]}, `
+      }
       newCard.appendChild(newCardName);
       newCard.appendChild(newCardBirthYear);
+      newCard.appendChild(newCardFilms);
       newCard.classList.add("charCards", "hidden")
       selectedCharCards.appendChild(newCard)
     }
   }
-
+// lyssnar på inputfielden på knapptryck. För varje knapp som trycks så jämför den inputvärdet med knapparnas textcontent. Tex om du skriver "lu" så kommer den hitta alla som har "lu" i knappens innertext.
   input.addEventListener("keyup", function(event) {
     const buttons = document.querySelectorAll(".charButtons") as NodeListOf<HTMLButtonElement>;
     if (!input.value) {
@@ -86,8 +116,9 @@ function buttonMaker() {
       }
     } else {
       for (let i = 0; i < buttons.length; i++) {
+        // här sker jämförelsen
         if (buttons[i].textContent && buttons[i].textContent?.toLowerCase().includes(input.value.toLowerCase())) {
-            
+            //så om if satsen hittar matching så tar den bort klassen hidden från knappen så att den visas i suggestions sectionen. Om en knapp inte matchar texten man skrivit in i inputfielden så får den hidden klassen
           buttons[i].classList.remove("hidden");
         } else {
           buttons[i].classList.add("hidden");
@@ -96,53 +127,20 @@ function buttonMaker() {
     }
   });
 
-
-
-
-  let lastClickedButton: HTMLButtonElement;
-
   function selectCharacters() {
     const charButtons = document.querySelectorAll(".charButtons") as NodeListOf<HTMLButtonElement>;
     const charCards = document.querySelectorAll(".charCards") as NodeListOf<HTMLDivElement>;
-    charButtons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const person = persons.find((p) => p.name === button.innerText);
   
-        if (person) {
-          input.value = "";
-            
-          charCards.forEach((card) => {
-            card.classList.add("hidden");
-            const cardName = card.querySelector("p:first-child") as HTMLParagraphElement;
-            if (cardName.innerText === person.name) {
-              card.classList.remove("hidden");
-            }
-          });
+    for (let i = 0; i < charButtons.length; i++) {
+      charButtons[i].onclick = function() {
+        // Hide all charCards
+        for (let j = 0; j < charCards.length; j++) {
+          charCards[j].style.display = "none";
         }
-      });
-    });
+        // Show the corresponding charCard
+        charCards[i].style.display = "block";
+      };
+    }
   }
-// TESTA SENARE 
-// const favoritKnapp = document.querySelector("#favoritKnapp") as HTMLButtonElement;
-
-// favoritKnapp.onclick = function() {
-//     console.log("hej");
-//     const favorites = document.getElementById("favourites") as HTMLDivElement;
-    
-//     if (lastClickedButton) {
-//         const copyButton = lastClickedButton.cloneNode(true) as HTMLButtonElement;
-//         copyButton.classList.remove("charButtons", "hidden");
-//         copyButton.classList.add("favCharacters");
-//         if (lastClickedButton.onclick) {
-//             copyButton.addEventListener("click", lastClickedButton.onclick);
-//         }
-//         favorites.appendChild(copyButton);
-//     }
-// }
-//----------------------------------------------------------------------------------------------------------//
-
-
-
-
 
 
